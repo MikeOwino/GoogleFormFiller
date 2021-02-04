7335b4b2c518b805f12a8322ad30d37be804af21
drieda <65320245+drieda@users.noreply.github.com>
2021-02-04 22:41:37 +0100

    Create index.js

---
+++ Beta 2.1/index.js
@@ -0,0 +1,41 @@
+const FormTypes = require('./fillerModules/formTypeCreator');
+const FormFiller = require('./fillerModules/formFiller');
+const Types = new FormTypes();
+
+const OPTIONS = {
+    URL: "https://docs.google.com/forms/d/e/1FAIpQLSdo727ovL-zbVIZZWr7_c5eyI-ubWc7br7441yjsrkjfkww9Q/viewform",
+    NR_OF_SUBMITS: 10,
+    
+    RANDOM_NAMES: false,
+    AUTOSUBMIT: true,
+    AUTOEXIT: true,
+    LOG_TO_FILE: false,
+
+    DEFAULT_TEXT_ANSWER: "Filled by Form Filler",
+
+    FORM_STRUCTURE: [
+        Types.Dropdown(0,5),
+        Types.ShortAnswer(""),
+        Types.Next(),
+        Types.MultipleChoice(0,3),
+        Types.Paragraph(),
+        Types.ShortAnswer(),
+        Types.Checkbox([],3),
+        Types.Dropdown(0,3),
+        Types.MultipleChoice(0,3),
+        Types.Paragraph(),
+        Types.Dropdown(0,3),
+        Types.Checkbox([],3),
+        Types.Next(),
+        Types.LinearScale(0,5),
+        Types.MultipleChoiceGrid(3,5),
+        Types.CheckboxGrid(3,4),
+        Types.MultipleChoiceGrid(2,3),
+        Types.CheckboxGrid(3,2,[[],[1,2],[]]),
+        Types.LinearScale(0,10),   
+        
+    ]
+}
+
+const Filler = new FormFiller(OPTIONS);
+Filler.start();
