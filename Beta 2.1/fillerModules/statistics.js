1f72c85a225c8540ec8c5e4eef37ada3be7aa6ee
drieda <65320245+drieda@users.noreply.github.com>
2021-02-04 22:42:26 +0100

    Create statistics.js

---
+++ Beta 2.1/fillerModules/statistics.js
@@ -0,0 +1,59 @@
+class Statistics{
+    constructor(){
+        this.attempts = 0;
+        this.totalTime = 0;
+
+        this.tmpStartTime;
+    }
+
+    _formatTimeString = (time) => {
+        let s = (time / 1000).toFixed(2);
+
+        /*
+        if(s > 60){
+
+            //Total time in minutes
+            let m = (s / 60).toFixed(0);
+
+            if(m > 60){
+
+                //Total time in hours
+                let h = (m / 60).toFixed(2);
+                return h + "h " + (m - h*60).toFixed(0) + "m " + (s - m*60 - h*60).toFixed(2) + "s";
+            }
+
+            return m + "m " + (s - m*60).toFixed(2) + "s";
+        }
+        */
+
+        return s + "s";
+    }
+
+    getStats = () =>{
+        console.log("-----------------\nSTATISTICS\n-----------------");
+
+        console.log(`Attempts: ${this.attempts}`);
+        console.log(`Total runtime: ${this._formatTimeString(this.totalTime)}`);
+        console.log(`Avg. runtime per fill: ${this._formatTimeString(this.totalTime / this.attempts)}`);
+    }
+
+    fillStart = () => {
+        this.tmpStartTime = Date.now();
+        this.attempts++;
+
+        console.log("-----------------\nNR: " + (this.attempts) + "\n-----------------");
+        console.log(`Fill ${this.attempts} started at ` + new Date().toLocaleString());
+    }
+
+    fillEnd = () => {
+        let thisTotal = Date.now() - this.tmpStartTime;
+        this.totalTime += thisTotal;
+
+        console.log(`Fill ${this.attempts} finished at ` + new Date().toLocaleString());
+        console.log("Runtime: " + this._formatTimeString(thisTotal));
+    }
+
+}
+
+
+module.exports = Statistics;
